e3c8953f0464dd67873fa2be7a33c63ca80b3483
Lyna Ngo <lynango5623@gmail.com>
2022-03-14 04:46:05 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1 className="my-class">Hello Dojo!</h1>
+      <h2 className="my-class">Things I need to do:</h2>
+      <li>Learn React</li>
+      <li>Climb Mt. Everest</li>
+      <li>Run a Marathon</li>
+      <li>Feed the Dogs</li>
+    </>
+  );
+}
+
+export default App;
